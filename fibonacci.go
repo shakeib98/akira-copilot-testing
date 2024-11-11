40a88d1ed825fb21498fae42943215f80074af74
akira <akira@drox.com>
2024-11-11 16:23:53 +0000

    add fibonacci algorithm in go

---
+++ fibonacci.go
@@ -0,0 +1,27 @@
+// fibonacci.go
+
+package main
+
+import "fmt"
+
+// Fibonacci function returns a slice containing the Fibonacci sequence up to n terms
+func Fibonacci(n int) []int {
+    if n <= 0 {
+        return []int{}
+    }
+    fibSeq := make([]int, n)
+    fibSeq[0] = 0
+    if n > 1 {
+        fibSeq[1] = 1
+        for i := 2; i < n; i++ {
+            fibSeq[i] = fibSeq[i-1] + fibSeq[i-2]
+        }
+    }
+    return fibSeq
+}
+
+func main() {
+    n := 10 // Example: Get the first 10 Fibonacci numbers
+    fibSeq := Fibonacci(n)
+    fmt.Println(fibSeq)
+}
